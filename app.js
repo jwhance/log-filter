feba52b76829a6e16ec811ff371b2629fc72d3a3
Joe Hance <jwh20@hotmail.com>
2021-03-24 05:01:13 -0400

    Update

--- app.js
+++ app.js
@@ -45,29 +45,69 @@ const getAllLogStreams = async (logGroup, startDate, endDate, prefix) => {
     return streams;
 }
 
-const getLogEvents = async (logGroup, logStream, startDate, endDate) => {
+const getLogEvents = async (logGroup, logStream, startDate, endDate, nextToken) => {
     var params = {
-        logGroupName: 'STRING_VALUE', /* required */
-        logStreamName: 'STRING_VALUE', /* required */
-        endTime: 'NUMBER_VALUE',
-        limit: 'NUMBER_VALUE',
-        nextToken: 'STRING_VALUE',
-        startFromHead: true || false,
-        startTime: 'NUMBER_VALUE'
-      };
+        logGroupName: logGroup,
+        logStreamName: logStream,
+        endTime: endDate,
+        startFromHead: true,
+        startTime: startDate
+    };
 
-      const logEvents = await cloudwatchlogs.getLogEvents(params).promise();
-      return logEvents;
+    if (nextToken) {
+        params.nextToken = nextToken;
+    }
+
+    const logEvents = await cloudwatchlogs.getLogEvents(params).promise();
+    return logEvents;
 }
 
-console.log('Reading log streams...');
+const getAllLogEventsFromStream = async (logGroup, logStream, startDate, endDate) => {
+    var events = [];
+    var nextToken = null;
+    var _events = null;
+
+    do {
+        _events = await getLogEvents(logGroup, logStream, startDate, endDate, nextToken);
+        if(_events.nextForwardToken != nextToken) {
+            nextToken = _events.nextForwardToken;
+        } else {
+            nextToken = null;
+        }
+
+        _events.events.map(e => events.push(e));
+    } while (nextToken);
+
+    return events;
+}
+
+const getAllLogEvents = async (logGroup, logStreams, startDate, endDate) => {
+    let events = [];
+
+    for (let stream =0; stream < logStreams.length; stream++) {
+        let _events = await getAllLogEventsFromStream(logGroup, logStreams[stream], startDate, endDate);
+        _events.map(e => events.push(e));
+    }
+
+    return events;
+}
+
+console.log('Getting log streams...');
 getAllLogStreams(argv.loggroup, startDate, endDate, argv.prefix)
-    .then(result => {
-        
+    .then(async result => {
+        console.log('Getting log events...');
+        return await getAllLogEvents(argv.loggroup, result, startDate, endDate);
     })
     .then(result => {
-        console.log(result);
-        console.log(startDate, endDate);
+        if (argv.filter) {
+            console.log('Filtering log events...');
+            return result.filter(e => e.message.includes(argv.filter));
+        } else {
+            return result;
+        }
+    })
+    .then(result => {
+        result.map(e => console.log(e));
     })
     .catch(error => console.error(error));
 
